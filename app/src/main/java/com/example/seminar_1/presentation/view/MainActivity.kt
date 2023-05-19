10f069f4afe85b5189f3cfb569212a08811498bd
amsdevelops <amsdevelops@gmail.com>
2023-05-19 09:56:19 +0300

    Added UI

--- app/src/main/java/com/example/seminar_1/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/seminar_1/presentation/view/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.seminar_1.presentation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.seminar_1.R
+import com.example.seminar_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            
+        }
     }
 }
